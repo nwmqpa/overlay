46da14f021ba9b01294b6436b6d14b5f6614f289
Thomas Nicollet <thomas.nicollet@epitech.eu>
2021-01-28 13:53:57 +0100

    :sparkles: Added some manifests key

---
+++ manifest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+for i in $(ls -F | grep '/')
+do
+	for v in $(ls -F $i | grep '/')
+	do
+		comm -23 <(ls $i$v | grep .ebuild | sort) <(cat $i$v/Manifest | grep EBUILD | cut -d ' ' -f2 | sort) | xargs -I {} sh -c "cd $i$v && sudo ebuild {} manifest"
+	done
+done
+
